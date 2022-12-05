38b0afd655c7c5fecb2ba4d3ca324de9bf0f62a9
echo <hujw77@gmail.com>
2022-12-05 14:58:40 +0800

    clean

--- packages/persistent-merkle-tree/test/hash.test.ts
+++ packages/persistent-merkle-tree/test/hash.test.ts
@@ -5,10 +5,7 @@ describe("hash", function () {
   it("hash and hashTwoObjects should be the same", () => {
     const root1 = Buffer.alloc(32, 1);
     const root2 = Buffer.alloc(32, 2);
-    console.log(toHex(root1))
-    console.log(toHex(root2))
     const root = hash(root1, root2);
-    console.log(toHex(root))
 
     const obj1 = uint8ArrayToHashObject(root1);
     const obj2 = uint8ArrayToHashObject(root2);
@@ -17,7 +14,3 @@ describe("hash", function () {
     expect(newRoot).to.be.deep.equal(root, "hash and hash2 is not equal");
   });
 });
-
-function toHex(bytes: Uint8Array): string {
-  return Buffer.from(bytes).toString("hex");
-}
